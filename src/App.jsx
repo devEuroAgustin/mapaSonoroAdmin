90cfa02d6301036a0a08b917ea7c3383d7abee1a
N1t4z0 <agusdelpercio@gmail.com>
2024-03-26 19:34:49 -0300

    solve img problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import  { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import logoAustral from './assets/Austral_LAB.png';
 
 function Login() {
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -55,7 +56,7 @@ function Login() {
     <div className="container">
 <div className="login-container">
       <div className='login-header'>
-        <img src="src\assets/Austral_LAB.png" alt="Austral Lab" className='logo-austral'/>
+      <img src={logoAustral} alt="Austral Lab" className='logo-austral'/>
         <div className='login-titles'>
           <h1 className="login-title">CMS Mapa Sonoro</h1>
           <h2 className="login-subtitle">Fundacion Alerce 3000</h2>
