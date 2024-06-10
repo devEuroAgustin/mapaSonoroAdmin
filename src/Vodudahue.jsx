9d82eea3e3a1770ea6c64d5129c3e5f1e33635d1
N1t4z0 <agusdelpercio@gmail.com>
2024-06-10 16:27:07 -0300

    backend

--- src/Vodudahue.jsx
+++ src/Vodudahue.jsx
@@ -11,8 +11,14 @@ function MapaMain() {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [modalData, setModalData] = useState(null);
     const [nameVisibility, setNameVisibility] = useState(true);
-    const [language, setLanguage] = useState('English'); // Nuevo estado para el idioma
+    const [language, setLanguage] = useState('English'); 
+    const [selectedButton, setSelectedButton] = useState('OTOÑO'); // Nuevo estado para el botón seleccionado
 
+    const handleButtonClick = (buttonName) => {
+        setSelectedButton(buttonName);
+    };
+
+    
     const handlePuntoClick = (punto) => {
       setModalData(punto);
       setIsModalOpen(true);
@@ -34,48 +40,111 @@ function MapaMain() {
     }, []);
 
     return (
-        <div className="dashboard">
-            <Header onLanguageChange={setLanguage} language={language} /> {/* Pasa setLanguage y language a Header */}
+      <div className="dashboard">
+        <Header onLanguageChange={setLanguage} language={language} />
 
-            <div className="fund-content">
-            {isLoading ? (
-                <Loader />
-            ) : (
-                <>
-                <div className='mapa'>
-                    {puntos.map((punto, index) => {
-                    let coordenadas = {};
-                    try {
-                        coordenadas = JSON.parse(punto.coordenadas);
-                    } catch (error) {
-                        console.error('Error al parsear las coordenadas:', error);
-                    }
-                    const dataNameAttribute = nameVisibility ? (language === 'English' ? punto.nombre_eng : punto.nombre_es) : undefined; // Usamos el idioma para determinar qué propiedad mostrar
-                    return (
-                        <div 
-                        key={index} 
-                        className="punto" 
-                        style={{top: coordenadas.top, left: coordenadas.left, backgroundImage: `url(${radioButton})`}} 
-                        data-name={dataNameAttribute}
-                        onClick={() => handlePuntoClick(punto)}
-                        ></div>
-                    );
-                    })}
-                    {isModalOpen && modalData && (
-                    <ModalMapa onClose={closeModal}>
-                        <div style={{display: "flex"}}>
-                        <img src={radioButtonBlack}  className='radioButton'/>
-                        <h2 className='nombre-modal-mapa'>{language === 'English' ? modalData.nombre_eng : modalData.nombre_es}</h2> {/* Usamos el idioma para determinar qué propiedad mostrar */}
-                        </div>
-                        <p className='descripcion-modal-mapa'>{language === 'English' ? modalData.descripcion_eng : modalData.descripcion_es}</p>
-                        {/* <p className='description-modal-mapa'>{modalData.descripcion_engg}</p> */}
-                    </ModalMapa>
-                    )}
-                </div>
-                </>
-            )}
-            </div>
+        <div className="fund-content">
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <>
+              <div className="mapa">
+                {puntos.map((punto, index) => {
+                  let coordenadas = {};
+                  try {
+                    coordenadas = JSON.parse(punto.coordenadas);
+                  } catch (error) {
+                    console.error("Error al parsear las coordenadas:", error);
+                  }
+                  const dataNameAttribute = nameVisibility
+                    ? language === "English"
+                      ? punto.nombre_eng
+                      : punto.nombre_es
+                    : undefined; // Usamos el idioma para determinar qué propiedad mostrar
+                  return (
+                    <div
+                      key={index}
+                      className="punto"
+                      style={{
+                        top: coordenadas.top,
+                        left: coordenadas.left,
+                        backgroundImage: `url(${radioButton})`,
+                      }}
+                      data-name={dataNameAttribute}
+                      onClick={() => handlePuntoClick(punto)}
+                    ></div>
+                  );
+                })}
+                {isModalOpen && modalData && (
+                  <ModalMapa onClose={closeModal}>
+                    <div>
+                      <img
+                        src="https://img.freepik.com/foto-gratis/santa-maddalena-cordillera-dolomitastirol-sur_661209-237.jpg"
+                        className="imgfond"
+                      />
+                      <p className="descripcion-modal-mapa">info de punto</p>
+                      <h2 className="nombre-modal-fund">
+                        {language === "English"
+                          ? modalData.nombre_eng
+                          : modalData.nombre_es}
+                      </h2>
+                    </div>
+                    <p className="descripcion-modal-mapa">
+                      {language === "English"
+                        ? modalData.descripcion_eng
+                        : modalData.descripcion_es}
+                    </p>
+                    <div className="buttons-container">
+                      <button
+                        className={`button-fund ${
+                          selectedButton === "OTOÑO" ? "selected" : ""
+                        }`}
+                        onClick={() => handleButtonClick("OTOÑO")}
+                      >
+                        OTOÑO
+                      </button>
+                      <button
+                        className={`button-fund ${
+                          selectedButton === "INVIERNO" ? "selected" : ""
+                        }`}
+                        onClick={() => handleButtonClick("INVIERNO")}
+                      >
+                        INVIERNO
+                      </button>
+                      <button
+                        className={`button-fund-xl ${
+                          selectedButton === "PRIMAVERA" ? "selected" : ""
+                        }`}
+                        onClick={() => handleButtonClick("PRIMAVERA")}
+                      >
+                        PRIMAVERA
+                      </button>
+                      <button
+                        className={`button-fund ${
+                          selectedButton === "VERANO" ? "selected" : ""
+                        }`}
+                        onClick={() => handleButtonClick("VERANO")}
+                      >
+                        VERANO
+                      </button>
+                    </div>
+                    <div style={{display:'flex'}}>
+                        <p className='day-moment'>Momento del día</p>
+                        <select name="" id="" className='select-fund'>
+                            <option value="">todos</option>
+                            <option value="">Mañana</option>
+                            <option value="">Tarde</option>
+                            <option value="">Noche</option>
+                        </select>
+                    </div>
+                    {/* <p className='description-modal-mapa'>{modalData.descripcion_engg}</p> */}
+                  </ModalMapa>
+                )}
+              </div>
+            </>
+          )}
         </div>
+      </div>
     );
 }
 
