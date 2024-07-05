b1ec29342ee37245c6d04344d02c3b9f0542e0a0
Agustin <agustinelizalde93@gmail.com>
2024-07-05 19:32:47 -0300

    Implementación del Loader en el model

--- src/ModalFund.jsx
+++ src/ModalFund.jsx
@@ -1,29 +1,38 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
-
+import Loader from "./utils/LoaderModal";
+import React, {useState} from 'react';
 
 function ModalMapa({ children, onClose,  }) {
     
-
-
+  const [isLoading, setIsLoading] = useState(true); 
 
     const handleBackgroundClick = (event) => {
         if (event.target === event.currentTarget) {
           onClose();
         }
       };
+
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 3000);
+
   return (
     <div className="modalmapa" onClick={handleBackgroundClick}>
-      <div className="modal-content-mapa">
-      <a onClick={onClose} style={{
-  display: 'flex',
-  textDecoration: 'none',
-  color: 'black',
-  cursor: 'pointer',
-    flexDirection: "row-reverse"
-}}><FontAwesomeIcon icon={faTimes} /></a>
-
-        {children}
+       <div className="modal-content-mapa">
+      {isLoading ? (
+          <Loader />
+        ) : (
+          <><a onClick={onClose} style={{
+            display: 'flex',
+            textDecoration: 'none',
+            color: 'black',
+            cursor: 'pointer',
+              flexDirection: "row-reverse"
+          }}><FontAwesomeIcon icon={faTimes} /></a>
+          
+                  {children}</>
+        )}
       </div>
     </div>
   );
